c5d6b2fbd4d9bd8b3582e32dbdfb8eecf21a6e76
VikasJaiswal1998 <vikasjaiswalinlko1998@gmail.com>
2022-02-12 14:33:12 +0530

    last commit

--- file system organiser/fo.js
+++ file system organiser/fo.js
@@ -1,6 +1,12 @@
+#!/usr/bin/env node
+// above  always be on top..
 // terminal se input lene ke liye array mei input lete hai process.argv mein
 const fs = require('fs')
 const path = require('path')
+const helpModule = require('../command/help')
+const treehelper = require('../command/tree')
+const organisehelper = require('../command/organise')
+
 let input = process.argv.slice(2) // 1 index par value dega 
 let command = input[0]
 // console.log(input)
@@ -9,51 +15,33 @@ let command = input[0]
 
 
 
-switch(command){
-    case 'tree' :
-        console.log('tree implemented')
+
+
+switch (command) {
+    case 'tree':
+        // console.log('tree implemented')
+        // treeFn(input[1]);
+        treehelper.treemodule(input[1]);
         break;
     case 'organise':
         // console.log('organise implemeted')
-        organisefn(input[1])
+        // organisefn(input[1])
+        organisehelper.organisemodule(input[1]);
         break;
-    case 'help' :
+    case 'help':
         // console.log('hey!!');
-        helpfn();
+        // helpfn();
+        helpModule.helpmodulecalled();
         break;
     default:
         console.log('please enter a valid command');
         break;
 }
-function helpfn(){
-    console.log(`list of all commands ->
-                        1- tree - node fo.js tree <dirpath >
-                        2- organise - node fo.js organise <dirpath >
-                        3- help - node fo.js help`
-    )
-}
 
 // we cann't get inside folder directory first we check for the inside path is  exist or not ;
 
-function organisefn(dirpath){
-    let destpath
-    if(dirpath==undefined){ // if doesn't pass any path 
-       console.log('please enter a valid directory path') ;
-       return;
-    }
-   let check = fs.existsSync(dirpath)  
-    if(check== true){
-        destpath = path.join(dirpath, 'organize_files') // make a folder inside a extension folder .. ->organize_files . 
-        let descheck = fs.existsSync(destpath); // if this file ia alreduy exist .
-        if(descheck == true) // already  exist ;
-        {
-            console.log("this file is already exist")
-        }
-        else{
-            fs.mkdirSync(destpath); // make directory of that file if doesn't exist ..
-        }
-    }
-  else{
-    console.log("Please enter a valid path which is exist in your computer");
-  }
-}
+
+
+// tree function implementation
+
+
